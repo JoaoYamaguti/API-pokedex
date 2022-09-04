34929cb4d4079f872b2682cef1d2e7291ad9454f
JoaoYamaguti <105678854+JoaoYamaguti@users.noreply.github.com>
2022-09-04 17:31:56 -0300

    finish list of evolve pokemons

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 let form = document.querySelector('form')
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
+    
+    let search = document.querySelector('#search').value
+    let infos = await searchForPokemon(search)
+    
+    let answerImg = document.querySelector('.img')
+    answerImg.style.backgroundImage = `url(${infos.pokeUrl})`
+
+    let pId = document.querySelector('#id')
+    pId.innerText = '#' + infos.txtId
+    
+    let pName = document.querySelector('#name')
+    pName.innerText = infos.txtName
+    
+    let pType = document.querySelector('#type')
+    pType.textContent = infos.txtType
+    
+    
+    let pokeSpecies = await (await fetch(`https://pokeapi.co/api/v2/pokemon-species/${search}/`)).json()
+    let evolveChainUrl = pokeSpecies.evolution_chain.url
+    let evolvChain = await (await fetch(evolveChainUrl)).json()
+    const chain = Array(evolvChain.chain)
+    
+    evolvInfos(chain)
 
     async function searchForPokemon (idSearch) {
         let resposta = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${idSearch}/`)).json()
-        console.log(resposta)
         const {sprites, id, name, types} = resposta
 
         let txtType = ''
@@ -20,49 +42,41 @@ form.addEventListener('submit', async (e) => {
         }
         return info
     }
+    
+    let listEvolv = document.querySelector('#listOfEvolv')
+    listEvolv.innerHTML = ""
+    
+    async function evolvInfos (list) {
+        await list.forEach(async e =>{
+            console.log(e)
+            const infos = await searchForPokemon(e.species.name)
+            console.log(infos)
+            listEvolv.appendChild(addLi(infos))
+            if (e.evolves_to !== '') {
+                evolvInfos(e.evolves_to)
+            }
+        })
+    }
 
-    let search = document.querySelector('#search').value
-    let infos = await searchForPokemon(search)
-    console.log(infos)
+    function addLi (infos) {
+        let li = document.createElement('li')
+        let spanImg = document.createElement('span')
+        spanImg.classList.add('img')
+        spanImg.style.backgroundImage = `url(${infos.pokeUrl})`
 
-    let answerImg = document.querySelector('.img')
-    answerImg.style.backgroundImage = `url(${infos.pokeUrl})`
+        let p = document.createElement('p')
+        let spanId = document.createElement('span')
+        spanId.textContent = '#' + infos.txtId
+        let spanName = document.createElement('span')
+        spanName.textContent = infos.txtName
+        spanName.classList.add('capitalize')
 
-    let pId = document.querySelector('#id')
-    pId.innerText = '#' + infos.txtId
+        p.appendChild(spanId)
+        p.innerHTML += ' | ' 
+        p.appendChild(spanName)
 
-    let pName = document.querySelector('#name')
-    pName.innerText = infos.txtName
-
-    let pType = document.querySelector('#type')
-    pType.textContent = infos.txtType
-
-
-//     fetch(`https://pokeapi.co/api/v2/pokemon-species/${search}/`).then(r => {return r.json()}).then(specie => {
-//         fetch(`${specie.evolution_chain.url}`).then(r => {return r.json()}).then(evolutions => {
-//             console.log(evolutions)
-
-//             let main = document.querySelector('main')
-//             let sectionEvolv = document.createElement('section')
-//             sectionEvolv.classList.add('evolution')
-//             let listEvolv = document.createElement('ul')
-
-            
-
-//             // do {
-//             //     evolutions.chain.evolves_to.forEach(e => {
-//             //         e.species.name
-//             //     })
-//             // } while (condition);
-
-//             sectionEvolv.appendChild(listEvolv)
-//             main.appendChild(sectionEvolv)
-//         })
-//     })
+        li.appendChild(spanImg)
+        li.appendChild(p)
+        return li
+    }
 })
-
-// function addLi () {
-//     let liEvolv = document.createElement('li')
-//     let spanImg = document.createElement('span')
-//     spanImg.style.backgroundImage = `url(${})`
-// }
