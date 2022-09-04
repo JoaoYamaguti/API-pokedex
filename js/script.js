a1bd65d189aebd05d37d2872ee170cb9d7fc8100
JoaoYamaguti <105678854+JoaoYamaguti@users.noreply.github.com>
2022-09-04 18:08:19 -0300

    refactory

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let form = document.querySelector('form')
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
+
+    let listEvolv = document.querySelector('#listOfEvolv')
+    listEvolv.innerHTML = ""
     
     let search = document.querySelector('#search').value
     let infos = await searchForPokemon(search)
@@ -17,7 +20,6 @@ form.addEventListener('submit', async (e) => {
     let pType = document.querySelector('#type')
     pType.textContent = infos.txtType
     
-    
     let pokeSpecies = await (await fetch(`https://pokeapi.co/api/v2/pokemon-species/${search}/`)).json()
     let evolveChainUrl = pokeSpecies.evolution_chain.url
     let evolvChain = await (await fetch(evolveChainUrl)).json()
@@ -26,31 +28,30 @@ form.addEventListener('submit', async (e) => {
     evolvInfos(chain)
 
     async function searchForPokemon (idSearch) {
-        let resposta = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${idSearch}/`)).json()
-        const {sprites, id, name, types} = resposta
-
-        let txtType = ''
-        types.forEach(element => {
-            txtType += element.type.name + ' '
-        })
-
-        let info = {
-            pokeUrl : sprites.front_default,
-            txtId : id,
-            txtName : name,
-            txtType : txtType
+        let resposta
+        try {
+            resposta = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${idSearch}/`)).json()
+            const {sprites, id, name, types} = resposta
+            let txtType = ''
+            types.forEach(element => {
+                txtType += element.type.name + ' '
+            })
+            let info = {
+                pokeUrl : sprites.front_default,
+                txtId : id,
+                txtName : name,
+                txtType : txtType
+            }
+            return info
         }
-        return info
+        catch {
+            alert('pokemon Not Found :/ ')
+        }
     }
-    
-    let listEvolv = document.querySelector('#listOfEvolv')
-    listEvolv.innerHTML = ""
-    
+        
     async function evolvInfos (list) {
         await list.forEach(async e =>{
-            console.log(e)
             const infos = await searchForPokemon(e.species.name)
-            console.log(infos)
             listEvolv.appendChild(addLi(infos))
             if (e.evolves_to !== '') {
                 evolvInfos(e.evolves_to)
@@ -63,20 +64,19 @@ form.addEventListener('submit', async (e) => {
         let spanImg = document.createElement('span')
         spanImg.classList.add('img')
         spanImg.style.backgroundImage = `url(${infos.pokeUrl})`
-
+        li.appendChild(spanImg)
+        
         let p = document.createElement('p')
         let spanId = document.createElement('span')
         spanId.textContent = '#' + infos.txtId
         let spanName = document.createElement('span')
         spanName.textContent = infos.txtName
         spanName.classList.add('capitalize')
-
         p.appendChild(spanId)
         p.innerHTML += ' | ' 
         p.appendChild(spanName)
-
-        li.appendChild(spanImg)
         li.appendChild(p)
+
         return li
     }
 })
