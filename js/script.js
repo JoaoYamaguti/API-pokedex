8fe59faa9c25c6f0756032e3c368719a00e9b846
JoaoYamaguti <105678854+JoaoYamaguti@users.noreply.github.com>
2022-09-03 22:33:13 -0300

    adding async & await to fetch

--- js/script.js
+++ js/script.js
@@ -1,25 +1,68 @@
 let form = document.querySelector('form')
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault()
 
+    async function searchForPokemon (idSearch) {
+        let resposta = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${idSearch}/`)).json()
+        console.log(resposta)
+        const {sprites, id, name, types} = resposta
+
+        let txtType = ''
+        types.forEach(element => {
+            txtType += element.type.name + ' '
+        })
+
+        let info = {
+            pokeUrl : sprites.front_default,
+            txtId : id,
+            txtName : name,
+            txtType : txtType
+        }
+        return info
+    }
+
     let search = document.querySelector('#search').value
-    fetch(`https://pokeapi.co/api/v2/pokemon/${search}/`).then(r => {return r.json()}).then(API => {
-        console.log(API)
-        let answerImg = document.querySelector('.img')
-        answerImg.style.backgroundImage = `url(${API.sprites.front_default})`
+    let infos = await searchForPokemon(search)
+    console.log(infos)
 
-        let pId = document.querySelector('#id')
-        pId.innerText = '#' + API.id
+    let answerImg = document.querySelector('.img')
+    answerImg.style.backgroundImage = `url(${infos.pokeUrl})`
 
-        let pName = document.querySelector('#name')
-        pName.innerText = API.name
+    let pId = document.querySelector('#id')
+    pId.innerText = '#' + infos.txtId
 
-        let pType = document.querySelector('#type')
-        pType.textContent = ""
-        API.types.forEach(element => {
-            let pokeType = element.type.name
-            pType.textContent += pokeType + ' '
-        });       
-    })
+    let pName = document.querySelector('#name')
+    pName.innerText = infos.txtName
+
+    let pType = document.querySelector('#type')
+    pType.textContent = infos.txtType
+
+
+//     fetch(`https://pokeapi.co/api/v2/pokemon-species/${search}/`).then(r => {return r.json()}).then(specie => {
+//         fetch(`${specie.evolution_chain.url}`).then(r => {return r.json()}).then(evolutions => {
+//             console.log(evolutions)
+
+//             let main = document.querySelector('main')
+//             let sectionEvolv = document.createElement('section')
+//             sectionEvolv.classList.add('evolution')
+//             let listEvolv = document.createElement('ul')
+
+            
+
+//             // do {
+//             //     evolutions.chain.evolves_to.forEach(e => {
+//             //         e.species.name
+//             //     })
+//             // } while (condition);
+
+//             sectionEvolv.appendChild(listEvolv)
+//             main.appendChild(sectionEvolv)
+//         })
+//     })
 })
 
+// function addLi () {
+//     let liEvolv = document.createElement('li')
+//     let spanImg = document.createElement('span')
+//     spanImg.style.backgroundImage = `url(${})`
+// }
