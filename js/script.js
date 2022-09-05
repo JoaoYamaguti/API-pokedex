8810c5a443ff0b97d6d64602fe30b31f2016614f
JoaoYamaguti <105678854+JoaoYamaguti@users.noreply.github.com>
2022-09-05 17:44:20 -0300

    Bug

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', async (e) => {
-    const formBtn = document.querySelector('button')
-    formBtn.disabled = true
     e.preventDefault()
-
+    formDisable(true)
+    try{
     let listEvolv = document.querySelector('#listOfEvolv')
     listEvolv.innerHTML = ""
     
@@ -27,60 +26,67 @@ form.addEventListener('submit', async (e) => {
     let evolvChain = await (await fetch(evolveChainUrl)).json()
     const chain = Array(evolvChain.chain)
     
-    evolvInfos(chain)
-
-    formBtn.disabled = false
-
-    async function searchForPokemon (idSearch) {
-        let resposta
-        try {
-            resposta = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${idSearch}/`)).json()
-            const {sprites, id, name, types} = resposta
-            let txtType = ''
-            types.forEach(element => {
-                txtType += element.type.name + ' '
-            })
-            let info = {
-                pokeUrl : sprites.front_default,
-                txtId : id,
-                txtName : name,
-                txtType : txtType
-            }
-            return info
-        }
-        catch {
-            alert('pokemon Not Found :/ ')
-        }
+    await evolvInfos(chain)
     }
-        
-    async function evolvInfos (list) {
-        await list.forEach(async e =>{
-            const infos = await searchForPokemon(e.species.name)
-            listEvolv.appendChild(addLi(infos))
-            if (e.evolves_to !== '') {
-                evolvInfos(e.evolves_to)
-            }
-        })
+    catch {
+        alert('pokemon Not Found :/ ')
+        formDisable(false)
+        return
     }
-
-    function addLi (infos) {
-        let li = document.createElement('li')
-        let spanImg = document.createElement('span')
-        spanImg.classList.add('img')
-        spanImg.style.backgroundImage = `url(${infos.pokeUrl})`
-        li.appendChild(spanImg)
-        
-        let p = document.createElement('p')
-        let spanId = document.createElement('span')
-        spanId.textContent = '#' + infos.txtId
-        let spanName = document.createElement('span')
-        spanName.textContent = infos.txtName
-        spanName.classList.add('capitalize')
-        p.appendChild(spanId)
-        p.innerHTML += ' | ' 
-        p.appendChild(spanName)
-        li.appendChild(p)
-
-        return li
-    }
+    formDisable(false)
 })
+async function searchForPokemon (idSearch) {
+    let resposta
+    resposta = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${idSearch}/`)).json()
+    const {sprites, id, name, types} = resposta
+    let txtType = ''
+    types.forEach(element => {
+        txtType += element.type.name + ' '
+    })
+    let info = {
+        pokeUrl : sprites.front_default,
+        txtId : id,
+        txtName : name,
+        txtType : txtType
+    }
+    return info   
+}
+    
+async function evolvInfos (list) {
+    let listEvolv = document.querySelector('#listOfEvolv')
+    await list.forEach(async e =>{
+        const infos = await searchForPokemon(e.species.name)
+        listEvolv.appendChild(addLi(infos))
+        if (e.evolves_to !== '') {
+            evolvInfos(e.evolves_to)
+        }
+    })
+}
+
+function addLi (infos) {
+    let li = document.createElement('li')
+    let spanImg = document.createElement('span')
+    spanImg.classList.add('img')
+    spanImg.style.backgroundImage = `url(${infos.pokeUrl})`
+    li.appendChild(spanImg)
+    
+    let p = document.createElement('p')
+    let spanId = document.createElement('span')
+    spanId.textContent = '#' + infos.txtId
+    let spanName = document.createElement('span')
+    spanName.textContent = infos.txtName
+    spanName.classList.add('capitalize')
+    p.appendChild(spanId)
+    p.innerHTML += ' | ' 
+    p.appendChild(spanName)
+    li.appendChild(p)
+
+    return li
+}
+
+function formDisable(bool) {
+    const formBtn = document.querySelectorAll('input')
+    formBtn.forEach(e => {
+        e.disabled = bool
+    })
+}
